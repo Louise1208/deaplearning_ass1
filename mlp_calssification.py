faee395f4bb24689c6b2743c4d7fcbf8974db200
xiangzhiping <xzp1208@163.com>
2023-03-13 01:03:39 +0000

    finish!!

--- mlp_calssification.py
+++ mlp_calssification.py
@@ -1,13 +1,15 @@
-import math
 import os
-# os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = '1'
-
-
 import numpy as np
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 from torch.hub import load_state_dict_from_url
+from torchcam.methods import SmoothGradCAMpp
+from torchvision.transforms.functional import to_pil_image
+from torchcam.utils import overlay_mask
+from PIL import Image
+import matplotlib.pyplot as plt
+import torch.optim as optim
 # always check your version
 print(torch.__version__)
 import torch
@@ -17,27 +19,70 @@ torch.device('mps')
 device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
 print('mps' if torch.backends.mps.is_available() else 'cpu')
 
-from PIL import Image
-import matplotlib.pyplot as plt
-import torch.optim as optim
-import torchvision.models as models
-# Load pretrained AlexNet
-model_alexnet = models.alexnet(pretrained=True)
-# Your model changes here - also print trainable parameters
-import copy
-# No frozen layers
-for param in model_alexnet.parameters():
-    param.requires_grad = True
-# Print trainable parameters
-for name, param in model_alexnet.named_parameters():
-    if param.requires_grad:
-        print(name)
-# Copy the model
-model_alexnet_c1=copy.deepcopy(model_alexnet)
+class CNN(nn.Module):
+    def __init__(self, num_classes=30):
+        super(CNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer4=nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(256 * 8 * 8, 512)
+        )
+        self.reLu1=nn.ReLU()
+        self.fc1 = nn.Linear(512, 1024)
+        self.reLu2=nn.ReLU()
 
-# Your changes here - also print trainable parameters
+        self.fc2 = nn.Linear(1024, num_classes)
+
+    def forward(self,x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out=self.layer4(out)
+        out=self.reLu1(out)
+        out = self.fc1(out)
+        out=self.reLu2(out)
+        out = self.fc2(out)
 
 
+        return out
+
+def built_model_cnn(CNN_model,learning_rate):
+    num_classes=30
+    # Set up the model, loss function, and optimizer
+    model_CNN = CNN_model(num_classes=num_classes).to(device)
+    loss_function_CNN = nn.CrossEntropyLoss()
+    optimizer_CNN = optim.SGD(model_CNN.parameters(), lr=learning_rate,momentum=0.9)
+    return model_CNN,loss_function_CNN,optimizer_CNN
+class CNNWithDropout(CNN):
+    def __init__(self, num_classes=30):
+        super(CNNWithDropout, self).__init__()
+        self.dropout = nn.Dropout(p=0.5)
+
+    def forward(self,x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        x = self.dropout(x)
+        out=self.layer4(out)
+        out=self.reLu1(out)
+        out = self.fc1(out)
+        out=self.reLu2(out)
+        out = self.fc2(out)
+        return out
 
  # load the classes
 file = open("./comp5623m-artificial-intelligence/class.txt", "r")
@@ -47,28 +92,6 @@ for line in file.readlines():
     contents.append(curLine[:])
 classes=[items[1] for items in contents]
 # print('classes:',classes)
-
-
-
-
-
-
-from torchvision.io import read_image
-# Your code here!
-from torchcam.methods import SmoothGradCAMpp
-from torchvision.transforms.functional import normalize, resize, to_pil_image
-from torchcam.utils import overlay_mask
-
-
-# # load CNN model
-# model_cnn=CNN(num_classes=30)
-# model_cnn.load_state_dict(torch.load('best_model_cnn.pth'))
-# print(type(model_cnn))
-# #
-# transform_cnn= transforms.Compose(
-#     [transforms.ToTensor(),
-#      transforms.Normalize(0.5,0.5)])
-
 
 
 
@@ -110,19 +133,9 @@ def get_gradcam(model,model_name, image_paths, transform):
             print('true class is: ', labels[i])
             print('correctly classified')
             pred+=1
-            if pred>4:
+            if pred>1:
                 break
-            # cam(model, input_tensor, image_tensor)
-
-        if predicted.item()==labels[i] and model_name == 'alexnet':
-            print('predicted class is: ', predicted.item())
-            print('true class is: ', labels[i])
-            print('image path is: ', image_path)
-            print('incorrectly classified')
-            pred+=1
-            if pred>4:
-                break
-            cam_extractor = SmoothGradCAMpp(model, target_layer='features.12')
+            cam_extractor = SmoothGradCAMpp(model, target_layer='layer3.3')
             # Preprocess your data and feed it to the model
             out = model(input_tensor)
             # Retrieve the CAM by passing the class index and the model output
@@ -135,19 +148,28 @@ def get_gradcam(model,model_name, image_paths, transform):
             plt.axis('off');
             plt.tight_layout();
             plt.show()
-
-
-# # load model_alex
-model_alex= model_alexnet
-model_alex.classifier[6] = torch.nn.Linear(4096, 30)
-model_alex.load_state_dict(torch.load('best_model_alex.pth'))
-model_alex
-print(type(model_alex))
-transform_alex= transforms.Compose(
-    [transforms.Resize((224,224)),
-     transforms.ToTensor(),
-     transforms.Normalize(0.5,0.5)])
+            # Show the ordinary image using the default viewer on your system
+            img = plt.imread(image_path)
+            plt.imshow(img)
+            plt.axis('off')
+            plt.show()
 
 
 
-get_gradcam(model_alex, 'alexnet',image_paths, transform_alex)
+# Load the saved model
+learning_rate=0.01
+model_cnn=CNNWithDropout(num_classes=30)
+model_cnn.load_state_dict(torch.load('best_model_cnn.pth'))
+print(type(model_cnn))
+# model_cnn.to(device)
+transform_cnn = transforms.Compose([
+    # transforms.RandomHorizontalFlip(p=0.1),
+    # transforms.RandomVerticalFlip(p=0.1),
+    # transforms.RandomRotation(10),
+    # transforms.RandomAffine(degrees=10, translate=(0.1,0.1), scale=(0.9,1.1)),
+    # transforms.ColorJitter(brightness=0.1,  saturation=0.1, hue=0.1),
+    transforms.ToTensor(),
+    transforms.Normalize(0.5, 0.5)
+])
+
+get_gradcam(model_cnn, 'cnn',image_paths, transform_cnn)
